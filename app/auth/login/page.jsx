a9b1d5e69c26b4cbb302b0a3faa4afccbd67efcc
EpheyHertz <mwain579@gmail.com>
2025-02-01 02:47:44 +0300

    added google login

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -213,7 +213,7 @@ export default function Login() {
                 GitHub
               </button>
               <Link
-                href="http://127.0.0.1:8000/auth/google"
+                href="https://angelhouslistingbackendapis.onrender.com/auth/google"
                 className="flex items-center justify-center p-3 border border-gray-200 dark:border-gray-600 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors duration-300"
               >
                 <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
