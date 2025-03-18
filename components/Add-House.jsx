465a2479ccbf4c581f2041690ee695c37dcd460a
EpheyHertz <mwain579@gmail.com>
2025-03-18 18:20:33 +0300

    Deposit addition

--- components/Add-House.jsx
+++ components/Add-House.jsx
@@ -219,12 +219,12 @@ export default function AddHousePage() {
                   <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">KES</span>
                   <input
                     type="number"
-                    name="price"
+                    name="deposit"
                     value={formData.deposit}
                     onChange={handleChange}
                     className="w-full pl-14 pr-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                     required
-                    placeholder="Enter amount"
+                    placeholder="Enter deposit"
                   />
                 </div>
               </div>
