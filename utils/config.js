4ab8b5e673d15159311f666110b89b80460faa57
EpheyHertz <mwain579@gmail.com>
2025-03-29 00:56:59 +0300

    Changed backend url

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
 // export const BASE_URL='https://angelhouslistingbackendapis.onrender.com/'
-// export const BASE_URL='https://digitaloceanapis.comradehomes.me/'
-export const BASE_URL='http://127.0.0.1:8000/'
+export const BASE_URL='https://digitaloceanapis.comradehomes.me/'
+// export const BASE_URL='http://127.0.0.1:8000/'
 // export const BASE_URL='https://xm7l402n-8000.uks1.devtunnels.ms/'
